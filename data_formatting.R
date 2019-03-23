2aa9a13834c3a51c3d70114a39f873e4de8c0410
Jeffrey Chase <jc7437a@student.american.edu>
2019-03-23 14:20:23 -0400

    first commit

---
+++ data_formatting.R
@@ -0,0 +1,32 @@
+library(readxl)
+library(tidyverse)
+
+all_data <- tibble()
+
+makeSafeNames <- function (headers) {
+  for (i in 1:length(headers)) {
+    headers[i] <- headers[i] %>%
+      stringr::str_trim(side="both") %>%
+      stringr::str_replace( '[ ]', '_') %>%
+      stringr::str_to_lower() 
+  }
+  return(headers) 
+}
+
+
+for (i in 2015:2018) {
+  data <- read_csv(paste0('data_files/',i, '.csv'))
+  names(data) <- makeSafeNames(names(data))
+  
+  data <- data %>%
+    select(c(country, happiness_score, happiness_rank)) %>%
+    mutate(year=i)
+           
+  all_data <- rbind(all_data, data)
+}
+
+
+all_data
+
+
+write_csv(all_data, "combined_data.csv")
