dab77815fc9a971c43b0a78f629e7d1f3f97fe1a
Jeffrey Chase <jc7437a@student.american.edu>
2019-03-24 10:11:24 -0400

    animated intro

--- Map.js
+++ Map.js
@@ -1,142 +1,139 @@
-window.onload = function () {
-  let center = new L.LatLng(0, 0);
-  const mymap = L.map('maparea').setView(center, 1.5);
+let center = new L.LatLng(0, 0);
+const mymap = L.map('maparea').setView(center, 1.5);
 
-  const cornerNE = L.LatLng(50, 180);
-  const cornerSW = L.LatLng(-50, -180);
-  const bounds = L.latLngBounds(cornerSW, cornerNE);
+const cornerNE = L.LatLng(50, 180);
+const cornerSW = L.LatLng(-50, -180);
+const bounds = L.latLngBounds(cornerSW, cornerNE);
 
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd',
-    maxZoom: 5,
-    maxBounds: bounds,
-    maxBoundsViscosity: 1.0,
-  }).addTo(mymap).setZIndex(1);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  subdomains: 'abcd',
+  maxZoom: 5,
+  maxBounds: bounds,
+  maxBoundsViscosity: 1.0,
+}).addTo(mymap).setZIndex(1);
 
 
-  Promise.all([
+Promise.all([
       d3.json('data/geo/countries.geo.json'),
       d3.csv('data/combined_data.csv')
     ])
-    .then(function (data) {
-      let info = data[1];
-      info = info.filter(function (d) {
-        return d.year === "2018";
-      });
+  .then(function (data) {
+    let info = data[1];
+    info = info.filter(function (d) {
+      return d.year === "2018";
+    });
 
-      let mapping = {};
+    let mapping = {};
 
-      info.forEach(function (d) {
-        mapping[d.country] = +d.happiness_score;
-      })
-
-      let fillScale = d3.scaleLinear()
-        .domain(d3.extent(info.map(function (d) {
-          return +d.happiness_score;
-        })))
-        .range([1, 0])
-
-      let getColor = function (d) {
-        if (d === undefined) {
-          return '#555';
-        } else {
-          return d3.interpolateMagma(fillScale(d));
-
-        }
-      }
-
-
-      function countryStyle(feature) {
-        return {
-          weight: 1,
-          opacity: 0.6,
-          color: '#ccc',
-          dashArray: '1',
-          fillOpacity: 0.7,
-          fillColor: getColor(mapping[feature.properties.name])
-        };
-      }
-
-      function countryStyleEmphasized(feature) {
-        return {
-          weight: 3,
-          opacity: 1,
-          color: 'white',
-          dashArray: '1',
-          fillOpacity: 0.7,
-          fillColor: getColor(mapping[feature.properties.name])
-        };
-      }
-
-      let countriesLayer = L.geoJson(data[0], {
-        style: countryStyle
-      });
-
-
-      countriesLayer.eachLayer(function (l) {
-        let polygon = l.feature.geometry;
-        let center;
-        let id = makeSafeId(l.feature.properties.name) + "-label";
-
-        if (polygon.type === "MultiPolygon") {
-          let largestArea = 0;
-          let largestPolygon;
-          for (let i = 0; i < polygon.coordinates.length; i++) {
-            let shape = turf.polygon(polygon.coordinates[i]);
-            let area = turf.area(shape);
-            if (area > largestArea) {
-              largestArea = area;
-              largestPolygon = shape;
-            }
-          }
-          center = turf.centroid(largestPolygon);
-        } else {
-          center = turf.centroid(polygon);
-        }
-        l.on('mouseover', function (e) {
-          let popup = L.popup()
-            .setLatLng(new L.LatLng(center.geometry.coordinates[1],
-              center.geometry.coordinates[0]))
-            .setContent(
-              "<a href='#" + id + "'>" +
-              "<h4>" + l.feature.properties.name + "</h4>" +
-              "<p> Happiness Value: <span class='popup-value'>" +
-              mapping[l.feature.properties.name] +
-              "</span></p></a>"
-            ).openOn(mymap);
-          this.setStyle({
-            fillOpacity: 1,
-            color: 'white',
-            weight: 4,
-            dashArray: null
-          });
-          d3.select("#" + id).dispatch('mouseover');
-
-        });
-        l.on('mouseout', function (e) {
-          countriesLayer.resetStyle(e.target);
-          this.closePopup();
-          d3.select("#" + id).dispatch('mouseout');
-
-          l.on('click', function (e) {
-            d3.select("#" + id).dispatch('click');
-            let scrolled = document.getElementById(id);
-            console.log('scroll');
-            scrolled.scrollIntoView();
-//            let newY = scrolled.getBoundingClientRect().top;
-//            window.scroll(0, newY);
-          })
-        });
-      });
-
-      countriesLayer.addTo(mymap);
-
-      L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_only_labels/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 5
-      }).addTo(mymap).setZIndex(10);
+    info.forEach(function (d) {
+      mapping[d.country] = +d.happiness_score;
     })
 
-}
+    let fillScale = d3.scaleLinear()
+      .domain(d3.extent(info.map(function (d) {
+        return +d.happiness_score;
+      })))
+      .range([1, 0])
+
+    let getColor = function (d) {
+      if (d === undefined) {
+        return '#555';
+      } else {
+        return d3.interpolateMagma(fillScale(d));
+
+      }
+    }
+
+
+    function countryStyle(feature) {
+      return {
+        weight: 1,
+        opacity: 0.6,
+        color: '#ccc',
+        dashArray: '1',
+        fillOpacity: 0.7,
+        fillColor: getColor(mapping[feature.properties.name])
+      };
+    }
+
+    function countryStyleEmphasized(feature) {
+      return {
+        weight: 3,
+        opacity: 1,
+        color: 'white',
+        dashArray: '1',
+        fillOpacity: 0.7,
+        fillColor: getColor(mapping[feature.properties.name])
+      };
+    }
+
+    let countriesLayer = L.geoJson(data[0], {
+      style: countryStyle
+    });
+
+
+    countriesLayer.eachLayer(function (l) {
+      let polygon = l.feature.geometry;
+      let center;
+      let id = makeSafeId(l.feature.properties.name) + "-label";
+
+      if (polygon.type === "MultiPolygon") {
+        let largestArea = 0;
+        let largestPolygon;
+        for (let i = 0; i < polygon.coordinates.length; i++) {
+          let shape = turf.polygon(polygon.coordinates[i]);
+          let area = turf.area(shape);
+          if (area > largestArea) {
+            largestArea = area;
+            largestPolygon = shape;
+          }
+        }
+        center = turf.centroid(largestPolygon);
+      } else {
+        center = turf.centroid(polygon);
+      }
+      l.on('mouseover', function (e) {
+        let popup = L.popup()
+          .setLatLng(new L.LatLng(center.geometry.coordinates[1],
+            center.geometry.coordinates[0]))
+          .setContent(
+            "<a href='#" + id + "'>" +
+            "<h4>" + l.feature.properties.name + "</h4>" +
+            "<p> Happiness Value: <span class='popup-value'>" +
+            mapping[l.feature.properties.name] +
+            "</span></p></a>"
+          ).openOn(mymap);
+        this.setStyle({
+          fillOpacity: 1,
+          color: 'white',
+          weight: 4,
+          dashArray: null
+        });
+        d3.select("#" + id).dispatch('mouseover');
+
+      });
+      l.on('mouseout', function (e) {
+        countriesLayer.resetStyle(e.target);
+        this.closePopup();
+        d3.select("#" + id).dispatch('mouseout');
+
+        l.on('click', function (e) {
+          d3.select("#" + id).dispatch('click');
+          let scrolled = document.getElementById(id);
+          console.log('scroll');
+          scrolled.scrollIntoView();
+          //            let newY = scrolled.getBoundingClientRect().top;
+          //            window.scroll(0, newY);
+        })
+      });
+    });
+
+    countriesLayer.addTo(mymap);
+
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_only_labels/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 5
+    }).addTo(mymap).setZIndex(10);
+  })
--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 "use strict";
-
+// global utils 
 function makeSafeId(text) {
     return text.replace(/[ \)\(,\.]/g, '-').toLowerCase();
   }
+
+(function(){
+  window.onload = function(){
+    document.querySelector('header').classList.add("intro");
+    document.querySelector('header p').classList.add("hidden");
+  }
+  
+  
+  window.onscroll = animate;
+  
+  function animate(ev) {
+    let top = window.scrollY;
+    console.log(top);
+    let p = document.querySelector("header p");
+    let h = document.querySelector("header");
+    if (window.scrollY > 400){
+      p.style.opacity = (top - 300) / 300
+    } 
+    if (window.scrollY > 800){
+      console.log('removing');
+      h.classList.remove('intro');
+      p.classList.add('gone');
+      setTimeout(function(){window.scrollTo(0, 0);}, 1000);
+      window.onscroll = null;
+    }
+    
+  }
+})();
